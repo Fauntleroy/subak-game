54aceb68d5ee884fd53b928781d854ff70b1c7fb
Timothy Kempf <tim@kempf.dev>
2024-05-11 22:08:08 -0700

    use cooler fonts

--- src/components/dialog/dialog.motion.ts
+++ src/components/dialog/dialog.motion.ts
@@ -1,18 +1,20 @@
 export const variants = {
   hidden: {
     opacity: 0,
+    scale: 0.5,
     y: '8px'
   },
   visible: {
     opacity: 1,
-    y: '4px'
+    scale: 1,
+    y: '0px'
   }
 };
 
 export const transition = {
   type: 'spring',
   stiffness: 1000,
-  damping: 100,
+  damping: 60,
   mass: 1,
   delay: 0.125
 };
