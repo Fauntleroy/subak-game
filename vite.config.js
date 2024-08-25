a73bf604dd8c7e7ee6279c49496a0bfefc01f1d9
Timothy Kempf <tim@kempf.dev>
2024-08-25 14:31:22 -0700

    just see if we can use the app at subak.kempf.dev independently remove lib build config move to docs dir

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,15 @@
-import { resolve } from 'path';
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'SubakGame',
-      fileName: 'subak-game'
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
+  resolve: {
+    alias: {
+      src: './src'
     }
+  },
+  build: {
+    outDir: 'docs'
   },
   plugins: [react()]
 });
